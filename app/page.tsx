1d42246fdcf0bb314063c3958d1e4f2a1f5b83b2
Yurii Dubnytskyi <yur.dub7@gmail.com>
2024-12-15 13:21:03 +0200

    add metadata and images

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h1 className="text-3xl text-light-800">Home</h1>;
+  return (
+    <>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Next.js</h1>
+      <h1 className="h1-bold font-inter">Welcome to Next.js (inter)</h1>
+    </>
+  );
 };
 
 export default Home;
