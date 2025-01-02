0147d35de75cc6c96aef9c21ce3d56ec05ac4909
Yurii Dubnytskyi <yur.dub7@gmail.com>
2025-01-02 16:16:49 +0200

    mobile side navbar

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import { Theme } from "./Theme";
+import { MobileNavigation } from "./MobileNavigation";
 
 export const Navbar = () => {
   return (
@@ -20,6 +21,7 @@ export const Navbar = () => {
       <p>Global Search</p>
       <div className="flex-between gap-5">
         <Theme />
+        <MobileNavigation />
       </div>
     </nav>
   );
--- constants/routes.ts
+++ constants/routes.ts
@@ -2,6 +2,12 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
+  COMMUNITY: "/community",
+  COLLECTIONS: "/collections",
+  JOBS: "/jobs",
+  TAGS: "/tags",
+  PROFILE: "/profile",
+  ASK: "/ask-question",
 };
 
 export default ROUTES;
