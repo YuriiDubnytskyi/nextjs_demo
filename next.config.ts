065fcd6dc203bc88bf5722b7064836468a28a162
Yurii Dubnytskyi <yur.dub7@gmail.com>
2025-01-10 09:16:21 +0200

    Implement Question Card

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   distDir: "build",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.vecteezy.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
